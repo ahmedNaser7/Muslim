60b04d4d96756ba46aa0a3bd634010e6a5a2316a
ad011 <ad0112727@gmail.com>
2024-01-25 00:28:39 +0200

    * Hadeth screen * Sura screen + Sebha tab

--- app/src/main/java/com/route/muslim/home/tabs/sebha/SebhaFragment.kt
+++ app/src/main/java/com/route/muslim/home/tabs/sebha/SebhaFragment.kt
@@ -1,13 +1,17 @@
 package com.route.muslim.home.tabs.sebha
 
+import android.graphics.Matrix
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView.ScaleType
+import androidx.fragment.app.Fragment
 import com.route.muslim.R
-import com.route.muslim.databinding.FragmentHadethBinding
 import com.route.muslim.databinding.FragmentSebhaBinding
+
 
 class SebhaFragment : Fragment() {
     private var _binding: FragmentSebhaBinding? = null
@@ -20,6 +24,30 @@ class SebhaFragment : Fragment() {
         _binding = FragmentSebhaBinding.inflate(layoutInflater, container, false)
         return binding?.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        val image = binding.sebhaBody
+        val btnCounter = binding.counter
+        val btn = binding.btn
+        var counter = 0
+        binding.btn.setOnClickListener {
+            val anim = AnimationUtils.loadAnimation(requireContext(), R.anim.rotate_animation)
+            image.startAnimation(anim)
+            if (counter == 33) {
+                counter = 0
+                btn.text = "الحمد لله"
+            }
+            counter++
+            btnCounter.text = counter.toString()
+        }
+
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
